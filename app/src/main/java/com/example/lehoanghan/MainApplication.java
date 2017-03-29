261e9ef83594065b42e4ed1b8394a4b37ae9eca2
DESKTOP-F1D8G5P\LeHoangHan <hoanghan.le.95@gmail.com>
2017-03-29 18:21:58 +0700

    update gradle (caligraphy +  joda time + dagger(only lib))

---
+++ app/src/main/java/com/example/lehoanghan/MainApplication.java
@@ -0,0 +1,31 @@
+package com.example.lehoanghan;
+
+import android.support.multidex.MultiDexApplication;
+
+import com.example.lehoanghan.appcalendar.R;
+
+import net.danlew.android.joda.JodaTimeAndroid;
+
+import org.androidannotations.annotations.EApplication;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+/**
+ * Created by LeHoangHan on 3/29/2017.
+ */
+@EApplication
+public class MainApplication extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        JodaTimeAndroid.init(this);
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Roboto-RobotoRegular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+}
