8a37c76bc1021dda23a7c7f1e82c4dd87844196d
DESKTOP-F1D8G5P\LeHoangHan <hoanghan.le.95@gmail.com>
2017-03-30 11:06:02 +0700

    Create MainApplication and add fonts

--- app/src/main/java/com/example/lehoanghan/MainApplication.java
+++ app/src/main/java/com/example/lehoanghan/MainApplication.java
@@ -1,9 +1,7 @@
 package com.example.lehoanghan;
 
 import android.support.multidex.MultiDexApplication;
-
 import com.example.lehoanghan.appcalendar.R;
-
 import net.danlew.android.joda.JodaTimeAndroid;
 
 import org.androidannotations.annotations.EApplication;
@@ -11,7 +9,7 @@ import org.androidannotations.annotations.EApplication;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
- * Created by LeHoangHan on 3/29/2017.
+ * ->  Created by LeHoangHan on 3/29/2017.
  */
 @EApplication
 public class MainApplication extends MultiDexApplication {
@@ -23,7 +21,7 @@ public class MainApplication extends MultiDexApplication {
         JodaTimeAndroid.init(this);
 
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
-                .setDefaultFontPath("fonts/Roboto-RobotoRegular.ttf")
+                .setDefaultFontPath("fonts/Roboto-Regular.ttf")
                 .setFontAttrId(R.attr.fontPath)
                 .build()
         );
